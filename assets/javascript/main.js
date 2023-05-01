af603630ece696865a4fa633b8447bb2d7f45aa1
Victória Martins <martinsvictoriaa@gmail.com>
2023-05-01 17:07:23 -0300

    night mode

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,9 @@
 const listaHTML = document.getElementById('lista-de-pokemons')
 const btnPaginacao = document.getElementById('load-more')
+const btnMode = document.getElementById('night-mode')
+const icon = document.getElementById('icon-moon')
+const body = document.getElementsByTagName('body')
+const h1 = document.getElementsByTagName('h1')
 const limit = 5
 let offset = 0
 const maxRecords = 151
@@ -42,6 +46,20 @@ function loadPokeItems(offset = 0, limit = 5) {
     })
 }
 
+function loadNightMode() {
+    h1[0].classList = []
+    body[0].classList = []
+    btnMode.classList.remove('light-mode')
+    icon.classList.remove('moon-light')
+}
+
+function loadLightMode() {
+    btnMode.classList.add('light-mode')
+    icon.classList.add('moon-light')
+    h1[0].classList.add('h1-light')
+    body[0].classList.add('body-light')
+}
+
 loadPokeItems(offset, limit)
 
 btnPaginacao.addEventListener('click', () => {
@@ -54,4 +72,12 @@ btnPaginacao.addEventListener('click', () => {
         loadPokeItems(offset, newLimit)
         btnPaginacao.parentElement.removeChild(btnPaginacao)
     } else loadPokeItems(offset, limit)
+})
+
+btnMode.addEventListener('click', () => {
+    if (btnMode.classList.length == 1) {
+        loadLightMode()
+    } else {
+        loadNightMode()
+    }
 })
